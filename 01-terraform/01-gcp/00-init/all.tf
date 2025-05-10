4d62136ecfa6c4192974a8465841b35cf7ea8d88
tribuiv01 <tribuiv01@gmail.com>
2025-05-10 02:07:58 +0000

    add

---
+++ 01-terraform/01-gcp/00-init/all.tf
@@ -0,0 +1,15 @@
+resource "google_project_service" "required_apis" {
+  for_each = toset([
+    "compute.googleapis.com",          # Compute Engine API
+    "iam.googleapis.com",              # Identity and Access Management API
+    "cloudresourcemanager.googleapis.com", # Cloud Resource Manager API
+    "serviceusage.googleapis.com",     # Service Usage API
+    "servicenetworking.googleapis.com" # Service Networking API
+  ])
+  
+  project = "prj-tribvcloud"
+  service = each.key
+  
+  disable_dependent_services = false
+  disable_on_destroy         = false
+}
